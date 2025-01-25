ce05f0844f9bc8344f3c1ee6ff6263e96c3681a1
Arash <arashsaleh798@gmail.com>
2025-01-24 17:16:04 -0800

    Exercise 12 is complete and sigh was nice and easier

--- app.js
+++ app.js
@@ -179,3 +179,19 @@ game.catchPokemon(pokemon[22]);
 
 console.log(game.items);
 
+/*
+Exercise 12
+1. Similar to Exercise 6, now complete gyms with a difficulty below 6. How will you approach this?
+ (change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 12 here:
+*/
+
+for (i = 0; i < game.gyms.length; i++) {
+    if (game.gyms[i].difficulty <= 6) {
+        game.gyms[i].completed = true;
+    }
+}
+
+console.log(game.gyms);
+
